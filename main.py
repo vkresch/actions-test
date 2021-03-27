510da33588190f6a504dcb86db67b7b55f30b2a3
Viktor Kreschenski <viktor.kreschenski@altran.com>
2021-03-27 15:21:35 +0100

    Continue dev

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ if __name__ == "__main__":
     print("This is a new feature which is awesome!")
     print("Another new feature implemented!")
     print("Something new")
+    print("continue")
     hello_list = important_function()
     print(hello_list)
